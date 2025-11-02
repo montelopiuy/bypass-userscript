1e54e937a2df5e0f1fd885419d0c056a965895c7
montelopiuy <105738292+montelopiuy@users.noreply.github.com>
2025-11-02 10:05:59 +0100

    Add files via upload

---
+++ Work_inkBypassByCAMPER.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Camper Wonk.Ink Cat
+// @namespace    http://tampermonkey.net/
+// @version      3.5
+// @author       CAMPER
+// @description  Work.ink bypass based on IHaxU
+// @match        *://work.ink/*
+// @match        *://*.work.ink/*
+// @match        *://loot-link.com/s?*
+// @match        *://loot-links.com/s?*
+// @match        *://lootlink.org/s?*
+// @match        *://lootlinks.co/s?*
+// @match        *://lootdest.info/s?*
+// @match        *://lootdest.org/s?*
+// @match        *://lootdest.com/s?*
+// @match        *://links-loot.com/s?*
+// @match        *://linksloot.net/s?*
+// @match        *://rekonise.com/*
+// @match        *://link-unlock*
+// @match        *://linkvertise.com/*/*
+// @match        *://key.volcano.wtf/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM.xmlHttpRequest
+// @include      *work.ink/*
+// @connect      trw.lat
+// @run-at       document-start
+// @require      https://github.com/Chaaan0917/Camper2.0/raw/refs/heads/main/Camper.js
+// @updateURL    https://github.com/Chaaan0917/Camper2.0/raw/refs/heads/main/Main2.0.user.js
+// @icon         https://i.kym-cdn.com/entries/icons/original/000/043/403/cover3.jpg
+// @grant        none
+// ==/UserScript==
+
+// set 30 if you are doing luarmor
+setDefaultCountdown(0);
